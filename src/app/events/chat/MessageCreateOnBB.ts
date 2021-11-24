4da089825d79c38629f81e6a7fa4d9f57d74b613
Behold <Behold.3th@gmail.com>
2021-11-23 23:48:13 -0500

    cleaned up tsc from yarn.lock

--- src/app/events/chat/MessageCreateOnBB.ts
+++ src/app/events/chat/MessageCreateOnBB.ts
@@ -2,7 +2,9 @@ import { Message } from 'discord.js';
 
 const MessageCreateOnBB = async (message: Message): Promise<any> => {
 	const greetings = ['Hello', 'Howdy', 'Hey', 'Go Bankless,', 'Nice to meet you,', 'It\'s great to see you,', 'Ahoy,'];
-	if (message.content.toLowerCase().match('^.*bounty board[!.?]$')) {
+	const bountyBoard = message.content.toLowerCase().match('^.*bounty board[!.?]*$');
+	const bountyBot = message.content.toLowerCase().match('^.*bountybot[!.?]*$');
+	if (bountyBoard || bountyBot) {
 		message.channel.send({ content: `${greetings[Math.floor(Math.random() * greetings.length)]} ${message.author.username}!` });
 	}
 	return;
